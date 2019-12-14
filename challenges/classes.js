4cda161a65fab91892924ba47c4db43cee6f724e
Wolfaholic <melissa.mackie@live.com>
2019-12-14 10:25:44 -0800

    finished sprint WTF I got it after sleeping

--- challenges/classes.js
+++ challenges/classes.js
@@ -12,14 +12,11 @@ class cuboidMaker {
 
     surfaceArea(){
     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
-      }
+    }
 }
 
-const cuboid = new cuboidMaker({
-    length: 4,
-    width: 5,
-    height: 5,
-  });
+const cuboid = new cuboidMaker(4 ,5, 5);
+    
       
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
